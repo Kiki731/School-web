7442efa218cd61e189e89641b6addc8e447c4a8a
Abass Okikioluwa <abassokikioluwa731@gmail.com>
2022-12-19 13:35:33 +0100

    This is the second commit which included the jscript

---
+++ js/signup.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $("#form-one").submit((event) => {
+        event.preventDefault();
+        const password = $("#word").val();
+    });
+});
+if (password.charAt(0).toUpperCase() !== password.charAt(0)){
+    return "uppercase joor"
+}
+$("#display").text(result)
